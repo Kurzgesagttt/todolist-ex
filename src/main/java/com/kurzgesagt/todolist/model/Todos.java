1c145f297291f420ef7a15a26b05b651c2e00eae
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-04-01 19:31:37 -0300

    entity

--- src/main/java/com/kurzgesagt/todolist/model/Todos.java
+++ src/main/java/com/kurzgesagt/todolist/model/Todos.java
@@ -1,7 +1,17 @@
 package com.kurzgesagt.todolist.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "todos")
 public class Todos {
+
+    @Column(name = "id")
+    private Long id;
+
+
+
+
 }
