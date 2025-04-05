e094830a0cae7901265b8ed91904dbc206626b5a
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-04-05 14:44:06 -0300

    getmap

--- src/main/java/com/kurzgesagt/todolist/controller/TodoController.java
+++ src/main/java/com/kurzgesagt/todolist/controller/TodoController.java
@@ -1,10 +1,13 @@
 package com.kurzgesagt.todolist.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@GetMapping("/")
 public class TodoController {
 
     @GetMapping
+    public ResponseEntity<>
 }
