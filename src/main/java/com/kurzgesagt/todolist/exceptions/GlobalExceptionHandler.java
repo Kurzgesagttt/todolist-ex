e045ab61a415f376b7944bef749c934061c9023c
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-04-07 13:54:23 -0300

    criando controle de excecao

---
+++ src/main/java/com/kurzgesagt/todolist/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.kurzgesagt.todolist.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
